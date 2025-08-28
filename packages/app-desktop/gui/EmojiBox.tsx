4b8faa696fe966b0692c6571cef65f3c5cb6e6df
Ying Li <yl966@students.waikato.ac.nz>
2025-08-28 15:36:13 +1200

    Desktop: Fixes [#12358]: EmojiBox: Calculate font size with hidden element before rendering to avoid oversized flash.([#12358].

--- packages/app-desktop/gui/EmojiBox.tsx
+++ packages/app-desktop/gui/EmojiBox.tsx
@@ -20,20 +20,22 @@ export default (props: Props) => {
 			return fontSizeCache_[cacheKey];
 		}
 
-		// Set the emoji font size start at 75% of the container height
-		// so new emojis don't overshoot
-
-		let spanFontSize = Math.round(props.height * 0.75);
-
+		// Create hidden element for calculation
 		const span = document.createElement('span');
 		span.innerText = props.emoji;
-		span.style.fontSize = `${spanFontSize}px`;
+		span.style.position = 'absolute';
+		span.style.visibility = 'hidden';
+		span.style.fontSize = `${props.height}px`;
+
+		// Append to container
 		containerRef.current.appendChild(span);
+
+		let spanFontSize = props.height;
 
 		let rect = span.getBoundingClientRect();
 
 		while (rect.height > props.height) {
-			spanFontSize -= .5;
+			spanFontSize -= 0.5;
 			span.style.fontSize = `${spanFontSize}px`;
 			rect = span.getBoundingClientRect();
 		}
@@ -42,7 +44,7 @@ export default (props: Props) => {
 
 		fontSizeCache_[cacheKey] = spanFontSize;
 		return spanFontSize;
-	}, [props.width, props.height, props.emoji, containerReady, containerRef]);
+	}, [props.width, props.height, props.emoji, containerReady]);
 
 	return <div className="emoji-box" ref={el => { containerRef.current = el; setContainerReady(true); }} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: props.width, height: props.height, fontSize }}>{props.emoji}</div>;
 };
